5273c3802a48da37e4ef78d0ac241e763adbdeec
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-12 17:45:27 -0500

    Initial commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ cal = input("Which category would you like to convert? we support length(l) and 
 if cal == "l":
     unit1 = input("Which unit would you like to convert from: ")
     unit2 = input("Which unit would you like to convert to: ")
-    num1 = input("Enter your value: ")
+    num1 = input("Enter the value of the unit: ")
 
     if unit1 == "cm" and unit2 == "m":
         ans = float(num1) / 100
@@ -26,3 +26,25 @@ if cal == "l":
         ans = float(num1) / 1000000
 
 
+if cal == "w":
+    unit1 = input("Which unit would you like to convert from: ")
+    unit2 = input("Which unit would you like to convert to: ")
+    num1 = input("Enter the value of the unit: ")
+    if unit1 == "kg" and unit2 == "lb":
+        ans = float(num1) * 2.205
+    elif unit1 == "lb" and unit2 == "kg":
+        ans = float(num1) / 2.205
+    elif unit1 == "oz" and unit2 == "g":
+        ans = float(num1) * 28.35
+    elif unit1 == "g" and unit2 == "oz":
+        ans = float(num1) / 28.35
+    elif unit1 == "kg" and unit2 == "g":
+        ans = float(num1) * 1000
+    elif unit1 == "g" and unit2 == "kg":
+        ans = float(num1) / 1000
+    elif unit1 == "km" and unit2 == "m":
+        ans = float(num1) * 1000
+    elif unit1 == "m" and unit2 == "km":
+        ans = float(num1) / 1000
+    elif unit1 == "mm" and unit2 == "km":
+        ans = float(num1) / 1000000
